8f82dd94ba8b7eda88b35fce592e8b5a0bdf2abe
Mint Missy <dawidkostka480@gmail.com>
2022-06-15 18:18:43 +0200

    Complete 4th project stage

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,10 +1,8 @@
 package readability;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,7 +11,8 @@ public class Main {
         List<Character> SENTENCE_END_CHARACTERS = new ArrayList<>(List.of('.', '?', '!'));
         int words = 0;
         int characters = 0;
-        double score = 0;
+        int syllables = 0;
+        int polysyllables = 0;
 
         String fileName = args[0];
         File file = new File(fileName);
@@ -27,16 +26,15 @@ public class Main {
         scanner.close();
 
         for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) != ' ' && text.charAt(i) != '\n' && text.charAt(i) != '\t'){
+            if (text.charAt(i) != ' ' && text.charAt(i) != '\n' && text.charAt(i) != '\t') {
                 characters++;
             }
         }
 
-
         // Get sentences
         int sentenceStartIndex = 0;
         List<String> sentences = new ArrayList<>();
-        for (int i = 0; i < text.length() -1; i++) {
+        for (int i = 0; i < text.length() - 1; i++) {
             if (SENTENCE_END_CHARACTERS.contains(text.charAt(i))) {
                 sentences.add(text.substring(sentenceStartIndex, i + 1));
                 sentenceStartIndex = i + 1;
@@ -47,70 +45,146 @@ public class Main {
         // Count words
         for (String sentence : sentences) {
             List<String> wordsInSentence = new ArrayList<>(List.of(sentence.split(" ")));
-            while (wordsInSentence.contains("") || wordsInSentence.contains(" ")){
+            while (wordsInSentence.contains("") || wordsInSentence.contains(" ")) {
                 wordsInSentence.remove("");
                 wordsInSentence.remove(" ");
+            }
+
+            for (String word : wordsInSentence) {
+                int syllablesInWord = countSyllables(word);
+                syllables += syllablesInWord;
+
+                if (syllablesInWord > 2) {
+                    polysyllables++;
+                }
             }
 
             words += wordsInSentence.size();
         }
 
         // Calculate score
-        score = (4.71 * ((double) characters / (double) words)) + (0.5 * ((double) words / (double) sentences.size())) - 21.43;
+        double ARIScore = calculateARIScore(characters, words, sentences.size());
+        double FKScore = calculateFKScore(words, sentences.size(), syllables);
+        double SMOGScore = calculateSMOGScore(polysyllables, sentences.size());
+        double CLScore = calculateCLScore(characters, words, sentences.size());
 
         System.out.println("Words: " + words);
         System.out.println("Sentences: " + sentences.size());
         System.out.println("Characters: " + characters);
-        System.out.println("The score is: " + score);
-        printTextAge(score);
-    }
+        System.out.println("Syllables: " + syllables);
+        System.out.println("Polysyllables: " + polysyllables);
 
-    private static void printTextAge(double score){
-        switch ((int) Math.ceil(score)){
-            case 1:
-                System.out.println("This text should be understood by 5-6-year-olds.");
+
+        System.out.print("Enter the score you want to calculate (ARI, FK, SMOG, CL, all): ");
+        Scanner consoleScanner = new Scanner(System.in);
+        String scoreToCalculate = consoleScanner.next();
+
+        switch (scoreToCalculate) {
+            case "ARI":
+                System.out.println("Automated Readability Index: (about " + ARIScore + " " + getTextAge(ARIScore) + ").");
                 break;
-            case 2:
-                System.out.println("This text should be understood by 6-7-year-olds.");
+            case "FK":
+                System.out.println("Flesch–Kincaid readability tests: (about " + FKScore + " " + getTextAge(FKScore) + ").");
                 break;
-            case 3:
-                System.out.println("This text should be understood by 7-9-year-olds.");
+            case "SMOG":
+                System.out.println("Simple Measure of Gobbledygook: (about " + SMOGScore + " " + getTextAge(SMOGScore) + ").");
                 break;
-            case 4:
-                System.out.println("This text should be understood by 9-10-year-olds.");
+            case "CL":
+                System.out.println("Coleman–Liau index: (about " + CLScore + " " + getTextAge(CLScore) + ").");
                 break;
-            case 5:
-                System.out.println("This text should be understood by 10-11-year-olds.");
-                break;
-            case 6:
-                System.out.println("This text should be understood by 11-12-year-olds.");
-                break;
-            case 7:
-                System.out.println("This text should be understood by 12-13-year-olds.");
-                break;
-            case 8:
-                System.out.println("This text should be understood by 13-14-year-olds.");
-                break;
-            case 9:
-                System.out.println("This text should be understood by 14-15-year-olds.");
-                break;
-            case 10:
-                System.out.println("This text should be understood by 15-16-year-olds.");
-                break;
-            case 11:
-                System.out.println("This text should be understood by 16-17-year-olds.");
-                break;
-            case 12:
-                System.out.println("This text should be understood by 17-18-year-olds.");
-                break;
-            case 13:
-                System.out.println("This text should be understood by 18-24-year-olds.");
-                break;
-            case 14:
-                System.out.println("This text should be understood by 24+-year-olds.");
+            case "all":
+                double avgScore = (ARIScore + FKScore + SMOGScore + CLScore) / 4;
+                System.out.println();
+                System.out.println("Automated Readability Index: (about " + ARIScore + " " + getTextAge(ARIScore) + ").");
+                System.out.println("Flesch–Kincaid readability tests: (about " + FKScore + " " + getTextAge(FKScore) + ").");
+                System.out.println("Simple Measure of Gobbledygook: (about " + SMOGScore + " " + getTextAge(SMOGScore) + ").");
+                System.out.println("Coleman–Liau index: (about " + CLScore + " " + getTextAge(CLScore) + ").");
+                System.out.println();
+                System.out.println("This text should be understood in average by " + getTextAge(avgScore) + ".");
                 break;
         }
+    }
 
+    private static double calculateARIScore(int characters, int words, int sentences) {
+        return (4.71 * ((double) characters / (double) words)) + (0.5 * ((double) words / (double) sentences)) - 21.43;
+    }
 
+    private static double calculateFKScore(int words, int sentences, int syllables) {
+        return 0.39 * ((double) words / (double) sentences) + 11.8 * ((double) syllables / (double) words) - 15.59;
+    }
+
+    private static double calculateSMOGScore(int polysyllables, int sentences) {
+        return 1.043 * Math.sqrt((double) polysyllables * (30 / (double) sentences)) + 3.1291;
+    }
+
+    private static double calculateCLScore(int characters, int words, int sentences) {
+        // L is the average number of characters per 100 words
+        double L = (100 * (double) characters) / (double) words;
+        // S is the average number of sentences per 100 words
+        double S = (100 * (double) sentences) / (double) words;
+
+        return 0.0588 * L - 0.296 * S - 15.8;
+    }
+
+    private static int countSyllables(String word) {
+        List<Character> VOWELS = new ArrayList<>(List.of('a', 'e', 'i', 'o', 'u', 'y'));
+
+        int vowels = 0;
+        boolean isPreviousLetterVowel = false;
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            boolean isLetterVowel = VOWELS.contains(letter);
+
+            if (!isLetterVowel) {
+                isPreviousLetterVowel = false;
+                continue;
+            }
+
+            if (isPreviousLetterVowel) {
+                continue;
+            }
+
+            vowels++;
+            isPreviousLetterVowel = true;
+        }
+
+        // If the last letter in the word is 'e' do not count it as a vowel
+        if (word.charAt(word.length() - 1) == 'e') {
+            vowels -= 1;
+        }
+
+        return vowels > 0 ? vowels : 1;
+    }
+
+    private static String getTextAge(double score) {
+        switch ((int) Math.ceil(score)) {
+            case 1:
+                return "6-year-olds";
+            case 2:
+                return "7-year-olds";
+            case 3:
+                return "9-year-olds";
+            case 4:
+                return "10-year-olds";
+            case 5:
+                return "11-year-olds";
+            case 6:
+                return "12-year-olds";
+            case 7:
+                return "13-year-olds";
+            case 8:
+                return "14-year-olds";
+            case 9:
+                return "15-year-olds";
+            case 10:
+                return "16-year-olds";
+            case 11:
+                return "17-year-olds";
+            case 12:
+                return "18-year-olds";
+            case 13:
+                return "24-year-olds";
+        }
+        return "24+-year-olds";
     }
 }
